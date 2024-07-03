b265bcde828cd4ac810d2e1a3e0c5920073087a1
Leonardo alvarado <leonardoalvarado673@gmail.com>
2024-07-02 20:10:00 -0600

    Primer commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del numero secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica un numero del 1 al 10';
+
+function intentoDeUsuario() {
+    alert('Click desde el boton')
+}
