355bf83c383151f3f7441ce63f7b2ddbf0100394
Leonardo alvarado <leonardoalvarado673@gmail.com>
2024-07-03 03:46:55 -0600

    aqctualiza app.js

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del numero secreto';
+let nuemroSecreto = generarNumeroSecreto();
+
+console.log(nuemroSecreto); 
 
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'Indica un numero del 1 al 10';
 
-function intentoDeUsuario() {
-    alert('Click desde el boton')
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
 }
+
+function verificarIntento() {
+let numeroDeUsuario = document.getElementById('valorUsuario').value;
+console.log(nuemroSecreto);    
+console.log(numeroDeUsuario);
+console.log(numeroDeUsuario == nuemroSecreto);
+return;
+}
+
+function generarNumeroSecreto() {
+   return Math.floor(Math.random()*10)+1;
+}
+
+asignarTextoElemento('h1','Juego del numero secreto');
+asignarTextoElemento('p','Indica un numero de 1 al 10');
